57129abee8798c1329e49fb74640be90d7991d36
bran <24.daw.brandoncarretero@fundacionmontessori.com>
2026-02-26 16:24:35 +0100

    Migraciones

---
+++ database/migrations/2026_02_25_153129_create_productos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('slug')->unique();
+            $table->text('descripcion')->nullable();
+            $table->decimal('precio', 10, 2);
+            $table->text('modo-empleo')->nullable();
+            $table->text('ingredientes_inci')->nullable();
+            $table->boolean('destacado')->default(false);
+            $table->boolean('activo')->default(true);
+
+            $table->foreignId('categoria_id')
+                ->constrained('categorias')
+                 ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
---
+++ database/migrations/2026_02_25_153510_create_pedidos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->foreignId('user_id')
+                ->constrained('users')
+                 ->OnDelete('cascade');
+
+            $table->enum('estado', ['pendiente', 'aceptado', 'cancelado'])
+                ->default('pendiente');
+
+            $table->enum('tipo',['normal','regalo'])
+                ->default('normal');
+
+            $table->string('tipo_envio')->nullable();
+            $table->string('forma_pago')->nullable();
+            $table->string('transaccion')->nullable();
+            $table->decimal('total',10,2)->default(0);
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pedidos');
+    }
+};
